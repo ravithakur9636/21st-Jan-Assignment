81181756f1e8193d91106e5514a13d6bc7476755
ravithakur9636 <thakurravi9636@gmail.com>
2022-01-23 01:26:39 +0530

    initial commit

---
+++ Assignment1.2/Assignment1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Enter a number:");
+            i = int.Parse(Console.ReadLine());
+            if(i%2==0)
+            {
+                Console.WriteLine("Entered number is an Even number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Entered number is an Odd number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
