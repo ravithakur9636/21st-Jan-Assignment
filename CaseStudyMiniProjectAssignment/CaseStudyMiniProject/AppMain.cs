d8b4674f4863364ff42aedb2a425e1e8ba2b5d38
ravithakur9636 <thakurravi9636@gmail.com>
2022-03-07 02:13:35 +0530

    initial commit

---
+++ CaseStudyMiniProjectAssignment/CaseStudyMiniProject/AppMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudyMiniProject
+{
+    class AppMain
+    {
+        static void Main(string[] args)
+        {
+            InterfaceImplementation interfaceImplementation = new InterfaceImplementation();
+            interfaceImplementation.showFirstScreen();
+
+            Console.ReadLine();
+        }
+    }
+}
