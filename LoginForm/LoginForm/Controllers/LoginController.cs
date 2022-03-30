f98f017a08d561e4930a3cd1cd4d442f85b7cee2
ravithakur9636 <thakurravi9636@gmail.com>
2022-03-30 17:48:36 +0530

    initial commit

---
+++ LoginForm/LoginForm/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginForm.Models;
+
+namespace LoginForm.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginDBEntities db = new LoginDBEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(user s)
+        {
+            if(ModelState.IsValid==true)
+            {
+                var credentials=db.users.Where(model=>model.username==s.username && model.password==s.password).FirstOrDefault();
+                if(credentials == null)
+                {
+                    ViewBag.ErrorMessage = "Login Failed";
+                    return View();  
+                }
+                else
+                {
+                    Session["username"]=s.username;
+                    return RedirectToAction("Index","Home");
+                }
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
+        }
+        public ActionResult Reset()
+        {
+            ModelState.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
