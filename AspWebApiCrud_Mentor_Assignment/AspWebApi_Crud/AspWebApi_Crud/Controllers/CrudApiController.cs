d47ea20523450ca5efee43ccb7c3ffd7a6c68232
ravithakur9636 <thakurravi9636@gmail.com>
2022-06-17 11:09:30 +0530

    initial commit

---
+++ AspWebApiCrud_Mentor_Assignment/AspWebApi_Crud/AspWebApi_Crud/Controllers/CrudApiController.cs
@@ -0,0 +1,64 @@
+﻿using AspWebApi_Crud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AspWebApi_Crud.Controllers
+{
+    public class CrudApiController : ApiController
+    {
+        MVC_Mentor_AssignmentEntities db=new MVC_Mentor_AssignmentEntities();
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetProducts()
+        {
+            List<Product> list=db.Products.ToList();
+            return Ok(list);    
+        }
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetProductById(int id)
+        {
+            var prod=db.Products.Where(model=>model.ProductID==id).FirstOrDefault();    
+            return Ok(prod);
+        }
+
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult ProdInsert(Product p)
+        {
+            db.Products.Add(p);
+            db.SaveChanges();   
+            return Ok();
+        }
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult ProdUpdate(Product p)
+        {
+            var prod= db.Products.Where(model=>model.ProductID==p.ProductID).FirstOrDefault();
+            if(prod != null)
+            {
+                prod.ProductID = p.ProductID;
+                prod.ProductName = p.ProductName;
+                prod.ProductPrice = p.ProductPrice;
+                prod.Category_id = p.Category_id;
+                prod.Unit_ID=p.Unit_ID;
+                db.SaveChanges();
+
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+        [System.Web.Http.HttpDelete]
+        public IHttpActionResult ProdDelete(int id)
+        {
+            var prod = db.Products.Where(model => model.ProductID == id).FirstOrDefault();
+            db.Entry(prod).State=System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ AspWebApiCrud_Mentor_Assignment/AspWebApi_Crud/AspWebApi_Crud/Controllers/CrudMvcController.cs
@@ -0,0 +1,151 @@
+﻿using AspWebApi_Crud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspWebApi_Crud.Controllers
+{
+    public class CrudMvcController : Controller
+    {
+        // GET: CrudMvc
+        HttpClient client = new HttpClient();
+        public ActionResult Index()
+        {
+            List<Product> prod_list = new List<Product>();
+            client.BaseAddress = new Uri("https://localhost:44354/api/CrudApi");
+            var response = client.GetAsync("CrudApi");
+            response.Wait();
+
+            var test=response.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Product>>();
+                display.Wait();
+                prod_list= display.Result;
+            }
+            return View(prod_list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product prod)
+        {
+            client.BaseAddress = new Uri("https://localhost:44354/api/CrudApi");
+            var response = client.PostAsJsonAsync<Product>("CrudApi", prod);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+        public ActionResult Add(Product s)
+        {
+            MVC_Mentor_AssignmentEntities db = new MVC_Mentor_AssignmentEntities();
+            db.Products.Add(s);
+            db.SaveChanges();
+            db.Dispose();
+            return Redirect("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Product p = null;
+            client.BaseAddress = new Uri("https://localhost:44354/api/CrudApi");
+            var response = client.GetAsync("CrudApi?id="+ id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Product>();
+                display.Wait();
+                p = display.Result;
+            }
+            return View(p);
+        }
+        public ActionResult Edit(int id)
+        {
+            Product p = null;
+            client.BaseAddress = new Uri("https://localhost:44354/api/CrudApi");
+            var response = client.GetAsync("CrudApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Product>();
+                display.Wait();
+                p = display.Result;
+            }
+            return View(p);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            client.BaseAddress = new Uri("https://localhost:44354/api/CrudApi");
+            var response = client.PutAsJsonAsync<Product>("CrudApi", p);
+            response.Wait();
+            var test=response.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+        }
+
+        public ActionResult Save(Product s)
+        {
+            MVC_Mentor_AssignmentEntities db= new MVC_Mentor_AssignmentEntities();
+            Product p=db.Products.Where(x=>x.ProductID==s.ProductID).FirstOrDefault();  
+            p.ProductName = s.ProductName;
+            p.ProductPrice = s.ProductPrice;    
+            p.Category_id=s.Category_id;
+            p.Unit_ID = s.Unit_ID;
+            db.SaveChanges();   
+            db.Dispose();
+            return Redirect("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Product p = null;
+            client.BaseAddress = new Uri("https://localhost:44354/api/CrudApi");
+            var response = client.GetAsync("CrudApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Product>();
+                display.Wait();
+                p = display.Result;
+            }
+            return View(p);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44354/api/CrudApi");
+            var response = client.DeleteAsync("CrudApi/"+ id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+            
+        }
+
+    }
+}
